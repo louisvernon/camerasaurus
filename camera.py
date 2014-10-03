83c4306ec468b22fc8edee5ef186c81f8d1810bc
vernon <louis.vernon@gmail.com>
2014-10-03 16:27:17 -0600

    moving to ffmpeg

--- camera.py
+++ camera.py
@@ -1,7 +1,11 @@
-import vlc
 import time
 import uuid
 import urllib
+import os
+import subprocess
+
+devnull = open(os.devnull, 'w')
+ffmpeg = "./ffmpeg"
 
 class Camera:
 	def __init__(self, ip, user, password, name, path):
@@ -12,14 +16,14 @@ class Camera:
 		self.path = path
 		
 	def start_recording(self):
-		filename = self.path + self.name + str(int(time.time())) + ".ogg"
+		filename = self.path + self.name + str(int(time.time())) + "_%04d.mkv"
 		rtsp_path = "rtsp://" + self.user + ":" + self.password + "@" + self.ip + ":554"
-		self.instance=vlc.Instance("--sout=file/avi:" + filename)
-		self.player = self.instance.media_player_new()
-		self.player.set_mrl(rtsp_path)
-		self.player.play()		
+		command = [ffmpeg, "-i", rtsp_path, "-codec", "copy",  "-map", "0", "-f", "segment", "-segment_time", "3600", filename]
+		print filename
+		self.recorder = subprocess.Popen(command, stdout=devnull, stderr=devnull)
+		
 	def stop_recording(self):
-		self.player.stop()
+		self.recorder.kill()	
 	def take_snapshot(self):
 		filename = self.path + self.name + str(int(time.time())) + ".jpg"
 		urllib.urlretrieve("http://" + self.user + ":" + self.password + "@" + self.ip + "/Streaming/Channels/1/picture", filename)
@@ -29,9 +33,7 @@ def record_stream(camera):
 
 
 
-camera = Camera(ip="192.168.1.107",user="admin",password="password",name="Garage",path="")
+camera = Camera(ip="192.168.1.86",user="admin",password="security",name="Front_Door",path="tmp/")
 camera.start_recording()
-time.sleep(5)
-camera.take_snapshot()
-time.sleep(5)
+time.sleep(100)
 camera.stop_recording()
