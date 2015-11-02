04518a82b357e84449247345e151a9a91ffeb5cb
Louis Vernon <louis.vernon@gmail.com>
2015-11-01 17:53:49 -0700

    Update camera class to better suit current ffmpeg

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ import uuid
 import urllib
 import os
 import subprocess
+import string
 
 devnull = open(os.devnull, 'w')
 ffmpeg = "ffmpeg"
@@ -15,31 +16,37 @@ class Camera:
 		self.password = password
 		self.name = name
 		self.path = path
-		
+
 	def start_recording(self):
 		filename = self.path + self.name + str(int(time.time())) + "_%04d.mkv"
 		rtsp_path = "rtsp://" + self.user + ":" + self.password + "@" + self.ip + ":" +  str(self.port)
-		command = [ffmpeg, "-i", rtsp_path, "-codec", "copy",  "-map", "0", "-f", "segment", "-segment_time", "3600", filename]
-		print filename
-		self.recorder = subprocess.Popen(command)
-		
+		#command = [ffmpeg, "-i", rtsp_path, "-codec", "copy",  "-map", "0", "-f", "segment", "-segment_time", "3600", filename]
+		command = '%s -i %s -codec copy %s' % (ffmpeg, rtsp_path, filename)
+		self.recorder = subprocess.Popen(string.split(command))
+
 	def stop_recording(self):
-		self.recorder.kill()
+		self.recorder.terminate()
+
 	def take_snapshot(self):
 		filename = self.path + self.name + str(int(time.time())) + ".jpg"
 		rtsp_path = "rtsp://" + self.user + ":" + self.password + "@" + self.ip + ":" +  str(self.port)
-		command = [ffmpeg, "-i", rtsp_path, "-vf", "select=\"eq(pict_type\,I)\"", "-vframes", "1", "-qscale", "0", filename]
-		print command
-		subprocess.Popen(command)
-		return filename		
-		
-def record_stream(camera):
-	print camera
+		command = '%s -i %s -vframes 1 -q:v 1 %s' % (ffmpeg, rtsp_path, filename)
+		subprocess.call(string.split(command))
+		return filename
 
 
 
-camera = Camera(ip="home.nxsfan.co.uk", port=25001, user="admin",password="security",name="Front_Door",path="tmp/")
+
+camera = Camera(ip="garage", port=554, user="admin",password="security",name="Front_Door",path="tmp/")
 #camera.start_recording()
 print camera.take_snapshot()
+
 time.sleep(2)
+
+camera.start_recording()
+
+time.sleep(5)
+
+camera.stop_recording()
+
 #camera.stop_recording()
